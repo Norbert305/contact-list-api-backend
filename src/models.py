de7054083e31be4d50ccd65261009bb56c7dd41f
Norbert305 <sealsnorbert@yahoo.com>
2022-03-02 15:14:01 +0000

    schema fix

--- src/models.py
+++ src/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), unique=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    address = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=True)
+    address = db.Column(db.String(120), unique=False, nullable=True)
+    phone = db.Column(db.String(80), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.full_name
